3ce1340061f41721c0032239c730e817f8cbcddb
khaled-dreat <dreatkhaled@gmail.com>
2024-05-10 23:14:07 +0300

    Refactor OnBoardingCubit for improved functionality and readability

--- lib/features/on_boarding/presentation/cubit/on_boarding_cubit.dart
+++ lib/features/on_boarding/presentation/cubit/on_boarding_cubit.dart
@@ -1,7 +1,37 @@
 import 'package:bloc/bloc.dart';
 
-part 'on_boarding_state.dart';
+class OnBoardingCubit extends Cubit<int> {
+  OnBoardingCubit() : super(0);
+  void nextIndex() {
+    if (state < 2) {
+      emit(state + 1);
+    }
+  }
 
-class OnBoardingCubit extends Cubit<OnBoardingState> {
-  OnBoardingCubit() : super(OnBoardingInitial());
+  String onBorderImages() {
+    List<String> imgList = [
+      "assets/svg/on_bording/1.svg",
+      "assets/svg/on_bording/2.svg",
+      "assets/svg/on_bording/3.svg",
+    ];
+    return imgList[state];
+  }
+
+  String onBordertitle() {
+    List<String> onBordertitle = [
+      "Welcome to Shoppify app",
+      "exclusive offer!",
+      "are you ready?",
+    ];
+    return onBordertitle[state];
+  }
+
+  String onBorderSuptitle() {
+    List<String> onBorderSuptitle = [
+      "Discover with us all the wonderful benefits it offers you.",
+      "Get details about exclusive offers and discounts available to new customers.",
+      "Let's start your journey with us!",
+    ];
+    return onBorderSuptitle[state];
+  }
 }

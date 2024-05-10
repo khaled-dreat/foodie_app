806f90fd2bd372ec059791d04c23d19ef23ad052
khaled-dreat <dreatkhaled@gmail.com>
2024-05-10 23:12:49 +0300

    Add constants for on-boarding SVG assets
    Added constants for on-boarding SVG assets in the AppImage class. Each constant
    represents a different SVG file path for on-boarding screens.

---
+++ lib/utils/constant/app_image.dart
@@ -0,0 +1,13 @@
+class AppImage {
+  /// Assets for svgOnBording1
+  /// assets/svg/on_bording/1.svg
+  static const String svgOnBording1 = "assets/svg/on_bording/1.svg";
+
+  /// Assets for svgOnBording2
+  /// assets/svg/on_bording/2.svg
+  static const String svgOnBording2 = "assets/svg/on_bording/2.svg";
+
+  /// Assets for svgOnBording3
+  /// assets/svg/on_bording/3.svg
+  static const String svgOnBording3 = "assets/svg/on_bording/3.svg";
+}

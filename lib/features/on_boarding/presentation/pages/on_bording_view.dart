1b06807a5d922cbcb1ba8fe4cb05fcb830922222
khaled-dreat <dreatkhaled@gmail.com>
2024-05-10 19:38:25 +0300

    Add OnBordingViewBody to OnBordingview body

--- lib/features/on_boarding/presentation/pages/on_bording_view.dart
+++ lib/features/on_boarding/presentation/pages/on_bording_view.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:foodie_app/features/on_boarding/presentation/pages/widgets/on_bording_view_body.dart';
 
 class OnBordingview extends StatelessWidget {
+  static const String nameRoute = "OnBordingview";
   const OnBordingview({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Scaffold(
+      body: OnBordingViewBody(),
+    );
   }
 }

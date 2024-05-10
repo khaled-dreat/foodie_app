9a3639b4742d49968b7cda31841acdfb1379d394
khaled-dreat <dreatkhaled@gmail.com>
2024-05-10 19:22:50 +0300

    Add bloc_connectivity plugin for managing connectivity in onboarding feature
    The bloc_connectivity plugin is added to manage connectivity in the onboarding
    feature. This plugin will help handle network connectivity related tasks
    effectively.

---
+++ lib/features/on_boarding/presentation/cubit/on_boarding_cubit.dart
@@ -0,0 +1,7 @@
+import 'package:bloc/bloc.dart';
+
+part 'on_boarding_state.dart';
+
+class OnBoardingCubit extends Cubit<OnBoardingState> {
+  OnBoardingCubit() : super(OnBoardingInitial());
+}
---
+++ lib/features/on_boarding/presentation/cubit/on_boarding_state.dart
@@ -0,0 +1,5 @@
+part of 'on_boarding_cubit.dart';
+
+abstract class OnBoardingState {}
+
+class OnBoardingInitial extends OnBoardingState {}

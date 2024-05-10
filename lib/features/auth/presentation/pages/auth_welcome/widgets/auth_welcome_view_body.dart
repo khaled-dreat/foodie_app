c05d4781189dc39a5977bdbf95ffa2f27849fab0
khaled-dreat <dreatkhaled@gmail.com>
2024-05-10 23:51:53 +0300

    Add AuthWelcomeViewBody widget for auth welcome page
    Add AuthWelcomeViewBody widget to display welcome message on the auth welcome page.

---
+++ lib/features/auth/presentation/pages/auth_welcome/widgets/auth_welcome_view_body.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AuthWelcomeViewBody extends StatelessWidget {
+  const AuthWelcomeViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.amberAccent,
+      child: Center(child: Text("data")),
+    );
+  }
+}

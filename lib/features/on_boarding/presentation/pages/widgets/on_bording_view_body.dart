727008f6e0f5ac71b15d0c45732359c5c9d5eec6
khaled-dreat <dreatkhaled@gmail.com>
2024-05-10 19:38:49 +0300

    Add OnBordingViewBody widget to on_boarding feature
    Add OnBordingViewBody widget to on_boarding feature for displaying on boarding view body.

---
+++ lib/features/on_boarding/presentation/pages/widgets/on_bording_view_body.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class OnBordingViewBody extends StatelessWidget {
+  const OnBordingViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("data"),
+    );
+  }
+}

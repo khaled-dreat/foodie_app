d618f52cc00c1db30bcb73932d5d0787d990ea8d
khaled-dreat <dreatkhaled@gmail.com>
2024-05-10 23:52:05 +0300

    Refactor image asset class names
    Update image asset class names for consistency and clarity. Also corrected file paths for onboarding images.

--- lib/utils/constant/app_image.dart
+++ lib/utils/constant/app_image.dart
@@ -1,13 +1,19 @@
-class AppImage {
+class Appimages {
+  Appimages._();
+
+  /// Assets for svgAuthWelcome
+  /// assets/svg/AuthWelcome.svg
+  static const String svgAuthWelcome = "assets/svg/AuthWelcome.svg";
+
   /// Assets for svgOnBording1
-  /// assets/svg/on_bording/1.svg
-  static const String svgOnBording1 = "assets/svg/on_bording/1.svg";
+  /// assets/svg/on_bording_1.svg
+  static const String svgOnBording1 = "assets/svg/on_bording_1.svg";
 
   /// Assets for svgOnBording2
-  /// assets/svg/on_bording/2.svg
-  static const String svgOnBording2 = "assets/svg/on_bording/2.svg";
+  /// assets/svg/on_bording_2.svg
+  static const String svgOnBording2 = "assets/svg/on_bording_2.svg";
 
   /// Assets for svgOnBording3
-  /// assets/svg/on_bording/3.svg
-  static const String svgOnBording3 = "assets/svg/on_bording/3.svg";
+  /// assets/svg/on_bording_3.svg
+  static const String svgOnBording3 = "assets/svg/on_bording_3.svg";
 }

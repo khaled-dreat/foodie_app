502b9673736b8f273249acb62f1890f8b569a24e
khaled-dreat <dreatkhaled@gmail.com>
2024-05-11 21:02:14 +0300

    Add CustomBtnAuth and CustomBtnAuthIcon widgets
    Added CustomBtnAuth and CustomBtnAuthIcon widgets for custom buttons with
    text and icon options in the authentication page.

---
+++ lib/features/auth/presentation/pages/widgets/custom_btn_auth.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:foodie_app/utils/constant/app_image.dart';
+import 'package:svg_flutter/svg_flutter.dart';
+
+class CustomBtnAuth extends StatelessWidget {
+  const CustomBtnAuth({
+    super.key,
+    required this.title,
+    required this.onPressed,
+    required this.backgroundColor,
+    required this.style,
+  });
+  final Color backgroundColor;
+  final String title;
+  final TextStyle style;
+
+  final void Function()? onPressed;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 310,
+      height: 65,
+      child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+              elevation: 0,
+              backgroundColor: backgroundColor,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12))),
+          onPressed: onPressed,
+          child: Text(
+            title,
+            style: style,
+          )),
+    );
+  }
+}
+
+class CustomBtnAuthIcon extends StatelessWidget {
+  const CustomBtnAuthIcon({
+    super.key,
+    required this.title,
+    required this.onPressed,
+    required this.backgroundColor,
+    required this.style,
+    required this.icon,
+  });
+  final Color backgroundColor;
+  final String title;
+  final String icon;
+  final TextStyle style;
+
+  final void Function()? onPressed;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 310,
+      height: 65,
+      child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+              elevation: 0,
+              backgroundColor: backgroundColor,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12))),
+          onPressed: onPressed,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SvgPicture.asset(
+                icon,
+                height: 25,
+              ),
+              const SizedBox(
+                width: 25,
+              ),
+              Text(
+                title,
+                style: style,
+              ),
+            ],
+          )),
+    );
+  }
+}

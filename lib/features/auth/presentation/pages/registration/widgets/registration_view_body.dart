8a572dae9a8901d426d5eb3bc23607c059e50f39
khaled-dreat <dreatkhaled@gmail.com>
2024-05-11 21:02:03 +0300

    Add registration form UI elements and styles
    Import necessary packages and add registration form UI elements
    with appropriate styling and layout. Include form fields for full
    name, email address, password, and registration button with Google
    sign-up option.

--- lib/features/auth/presentation/pages/registration/widgets/registration_view_body.dart
+++ lib/features/auth/presentation/pages/registration/widgets/registration_view_body.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:foodie_app/features/auth/presentation/pages/widgets/custom_btn_auth.dart';
+import 'package:foodie_app/utils/constant/app_image.dart';
+import 'package:foodie_app/utils/theme/app_colors.dart';
+import 'package:foodie_app/utils/theme/app_text_style.dart';
+import 'package:svg_flutter/svg_flutter.dart';
 import '../../widgets/custom_auth_text_form_field.dart';
 
 class RegistrationViewBody extends StatelessWidget {
@@ -8,8 +13,59 @@ class RegistrationViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.symmetric(horizontal: 15),
-      child: const Column(
-        children: [CustomAuthTextFormField()],
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          SvgPicture.asset(AppImages.svgAuthWelcome),
+          const SizedBox(
+            height: 20,
+          ),
+          const CustomAuthTextFormField(
+            isPass: false,
+            hintText: "Enter your full name",
+            label: "Full Name",
+          ),
+          const SizedBox(
+            height: 25,
+          ),
+          const CustomAuthTextFormField(
+            isPass: false,
+            hintText: "Eg namaemail@emailkamu.com",
+            label: "Email address",
+          ),
+          const SizedBox(
+            height: 25,
+          ),
+          const CustomAuthTextFormField(
+            isPass: true,
+            hintText: "**** **** ****",
+            label: "Password",
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          CustomBtnAuth(
+              title: "Registration",
+              onPressed: () {},
+              backgroundColor: AppColors.green,
+              style: AppTextStyle.styleBold14(context)
+                  .copyWith(color: AppColors.white)),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 100),
+            child: Divider(),
+          ),
+          CustomBtnAuthIcon(
+              title: "Sign up with Google",
+              onPressed: () {},
+              icon: AppImages.svgGoogle,
+              backgroundColor: AppColors.lightGray2,
+              style: AppTextStyle.styleBold14(context)
+                  .copyWith(color: AppColors.black)),
+          const SizedBox(
+            height: 80,
+          ),
+        ],
       ),
     );
   }

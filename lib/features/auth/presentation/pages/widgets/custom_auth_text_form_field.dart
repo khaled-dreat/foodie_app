54f9e3e0500b684032e493848cbacf57a435347c
khaled-dreat <dreatkhaled@gmail.com>
2024-05-11 21:02:09 +0300

    Update CustomAuthTextFormField widget design
    Refactor CustomAuthTextFormField widget to improve design and functionality.
    Added label, hintText, and isPass parameters for customization.

--- lib/features/auth/presentation/pages/widgets/custom_auth_text_form_field.dart
+++ lib/features/auth/presentation/pages/widgets/custom_auth_text_form_field.dart
@@ -1,20 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:foodie_app/utils/theme/app_text_style.dart';
 
 import '../../../../../utils/theme/app_colors.dart';
 
 class CustomAuthTextFormField extends StatelessWidget {
   const CustomAuthTextFormField({
     super.key,
+    required this.hintText,
+    required this.label,
+    required this.isPass,
   });
-
+  final String hintText, label;
+  final bool isPass;
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-        decoration: InputDecoration(
-      label: Text("data"),
-      enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(15),
-          borderSide: BorderSide(color: AppColors.silver)),
-    ));
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 8.0),
+          child: Text(
+            label,
+            style: AppTextStyle.styleSemiBold16(context),
+          ),
+        ),
+        TextFormField(
+            obscureText: isPass,
+            obscuringCharacter: '*',
+            decoration: InputDecoration(
+              hintText: hintText,
+              hintStyle: AppTextStyle.styleRegular13(context),
+              focusedBorder: outlineInputBorder(),
+              enabledBorder: outlineInputBorder(),
+            )),
+      ],
+    );
+  }
+
+  OutlineInputBorder outlineInputBorder() {
+    return OutlineInputBorder(
+        borderRadius: BorderRadius.circular(15),
+        borderSide: BorderSide(color: AppColors.silver));
   }
 }

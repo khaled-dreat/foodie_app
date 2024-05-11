40e7fd0dfb8f6dcf002c2408a30632f0da29713e
khaled-dreat <dreatkhaled@gmail.com>
2024-05-11 10:34:15 +0300

    Add RegistrationView widget to auth feature
    Add RegistrationView widget to auth feature with basic structure and route name.

---
+++ lib/features/auth/presentation/pages/registration/registration_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class RegistrationView extends StatelessWidget {
+  static const String nameRoute = "RegistrationView";
+  const RegistrationView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

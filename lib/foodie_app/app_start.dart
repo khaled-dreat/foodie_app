3e2bb7f76aabcf7a613a039ffb7f831fc5f25f2d
khaled-dreat <dreatkhaled@gmail.com>
2024-05-12 22:29:48 +0300

    Add WrapperCubit provider to MultiBlocProvider
    Add WrapperCubit provider to MultiBlocProvider for managing state in the app. This
    ensures that the WrapperCubit is available throughout the application.

--- lib/foodie_app/app_start.dart
+++ lib/foodie_app/app_start.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:foodie_app/features/auth/presentation/maneg/wrapper/wrapper_cubit.dart';
+import 'package:foodie_app/features/auth/presentation/pages/wrapper/widgets/wrapper_stream_builder.dart';
+import 'package:nested/nested.dart';
 import '../utils/route/app_route.dart';
 import '../utils/theme/app_theme_choose.dart';
 
@@ -9,15 +12,28 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      // * Theme
-      theme: AppThemeChoose.light(context),
-      //   darkTheme: AppThemeChoose.dark(context),
-      themeMode: ThemeMode.light,
-      // * Route
-      routes: AppRoute.routes,
-      initialRoute: AppRoute.inteRoute,
+    return MultiBlocProvider(
+      providers: providers,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        // * Theme
+        theme: AppThemeChoose.light(context),
+        //   darkTheme: AppThemeChoose.dark(context),
+        themeMode: ThemeMode.light,
+        // * Route
+        routes: AppRoute.routes,
+        initialRoute: AppRoute.inteRoute,
+      ),
     );
   }
+}
+
+List<SingleChildWidget> get providers {
+  return [
+    BlocProvider(
+      create: (context) {
+        return WrapperCubit();
+      },
+    ),
+  ];
 }

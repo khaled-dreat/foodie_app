02c98f769c32453d438b883de3841b46f5df1635
khaled-dreat <dreatkhaled@gmail.com>
2024-05-15 10:14:29 +0300

    Add resizeToAvoidBottomInset property to Scaffold widget
    Set the resizeToAvoidBottomInset property to false in the Scaffold widget to prevent the view from resizing when the keyboard appears.

--- lib/features/auth/presentation/pages/login/login_view.dart
+++ lib/features/auth/presentation/pages/login/login_view.dart
@@ -8,6 +8,7 @@ class LoginView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
+      resizeToAvoidBottomInset: false,
       body: LoginViewBody(),
     );
   }

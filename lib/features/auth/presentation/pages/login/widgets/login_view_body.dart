75b41970771dab5585eb448d63c01bed21beadb0
khaled-dreat <dreatkhaled@gmail.com>
2024-05-15 10:14:49 +0300

    Refactor login view body widget
    Extracted custom button with bloc consumer for reusability and better
    separation of concerns in the login view body widget. Updated button
    functionality to handle authentication states.

--- lib/features/auth/presentation/pages/login/widgets/login_view_body.dart
+++ lib/features/auth/presentation/pages/login/widgets/login_view_body.dart
@@ -1,75 +1,146 @@
 import 'package:flutter/material.dart';
+import 'package:foodie_app/features/auth/presentation/maneg/auth/auth_cubit.dart';
+import 'package:foodie_app/features/auth/presentation/pages/forget_pass/forget_pass_view.dart';
+import 'package:foodie_app/features/auth/presentation/pages/widgets/auth_field_email.dart';
+import 'package:foodie_app/features/auth/presentation/pages/registration/registration_view.dart';
+import 'package:foodie_app/features/home/presentation/pages/home/home_view.dart';
+import 'package:foodie_app/utils/route/app_route.dart';
 import 'package:svg_flutter/svg_flutter.dart';
 
 import '../../../../../../utils/constant/app_image.dart';
 import '../../../../../../utils/theme/app_colors.dart';
 import '../../../../../../utils/theme/app_text_style.dart';
-import '../../widgets/custom_auth_text_form_field.dart';
+import '../../../../../../utils/widgets/toast/app_toast.dart';
+import '../../widgets/auth_field_pass.dart';
+import '../../widgets/auth_footer.dart';
 import '../../widgets/custom_btn_auth.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class LoginViewBody extends StatelessWidget {
   const LoginViewBody({super.key});
+  static final GlobalKey<FormState> keyForm = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 15),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          SvgPicture.asset(AppImages.svgAuthWelcome),
-          const SizedBox(
-            height: 20,
-          ),
-          const CustomAuthTextFormField(
-            isPass: false,
-            hintText: "Eg namaemail@emailkamu.com",
-            label: "Email address",
-          ),
-          const SizedBox(
-            height: 25,
-          ),
-          const CustomAuthTextFormField(
-            isPass: true,
-            hintText: "**** **** ****",
-            label: "Password",
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 200),
-            child: TextButton(
-                onPressed: () {},
-                child: Text(
-                  "Forget Password?",
-                  style: AppTextStyle.styleSemiBold16(context)
-                      .copyWith(color: AppColors.green),
-                )),
-          ),
-          const SizedBox(
-            height: 50,
-          ),
-          CustomBtnAuth(
-              title: "Login",
-              onPressed: () {},
-              backgroundColor: AppColors.green,
-              style: AppTextStyle.styleBold14(context)
-                  .copyWith(color: AppColors.white)),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 100),
-            child: Divider(),
-          ),
-          CustomBtnAuthIcon(
-              title: "Login with Google",
-              onPressed: () {},
-              icon: AppImages.svgGoogle,
-              backgroundColor: AppColors.lightGray2,
-              style: AppTextStyle.styleBold14(context)
-                  .copyWith(color: AppColors.black)),
-          const SizedBox(
-            height: 80,
-          ),
-        ],
+    return Form(
+      key: keyForm,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            // * Image
+            SvgPicture.asset(AppImages.svgAuthWelcome),
+            const SizedBox(
+              height: 20,
+            ),
+            // * Text Form Filed Email
+            const AuthFieldEmail(),
+            const SizedBox(
+              height: 25,
+            ),
+            // * Text Form Filed Password
+            const AuthFieldPass(),
+            // * Forget Password Text Button
+            Padding(
+              padding: const EdgeInsets.only(left: 200),
+              child: TextButton(
+                  onPressed: () {
+                    AppRoute.go(context, ForgetPassView.nameRoute);
+                  },
+                  child: Text(
+                    "Forget Password?",
+                    style: AppTextStyle.styleSemiBold16(context)
+                        .copyWith(color: AppColors.green),
+                  )),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            // * Login Btn
+            CustomBtnAuthBlocConsumer(
+                keyForm: keyForm,
+                title: "Login",
+                onPressed: () async {
+                  if (keyForm.currentState?.validate() ?? false) {
+                    // ✅
+
+                    keyForm.currentState?.save();
+                    await context.read<AuthCubit>().authMethod();
+                  }
+                },
+                backgroundColor: AppColors.green,
+                style: AppTextStyle.styleBold14(context)
+                    .copyWith(color: AppColors.white)),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 100),
+              child: Divider(),
+            ),
+            // * Login with Google Btn
+            CustomBtnAuthIcon(
+                title: "Login with Google",
+                onPressed: () async {
+                  // ✅
+                  keyForm.currentState?.save();
+                  await context.read<AuthCubit>().signInWithGoogle();
+                },
+                icon: AppImages.svgGoogle,
+                backgroundColor: AppColors.lightGray2,
+                style: AppTextStyle.styleBold14(context)
+                    .copyWith(color: AppColors.black)),
+            const SizedBox(
+              height: 20,
+            ),
+            // * Auth Footer (Register)
+            AuthFooter(
+                onTap: () {
+                  AppRoute.go(context, RegistrationView.nameRoute);
+                },
+                first: "Don't have an account? ",
+                second: "Register"),
+            const SizedBox(
+              height: 60,
+            ),
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class CustomBtnAuthBlocConsumer extends StatelessWidget {
+  const CustomBtnAuthBlocConsumer(
+      {super.key,
+      required this.keyForm,
+      required this.title,
+      required this.backgroundColor,
+      required this.style,
+      this.onPressed});
+  final GlobalKey<FormState> keyForm;
+  final String title;
+  final Color backgroundColor;
+  final TextStyle style;
+  final void Function()? onPressed;
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<AuthCubit, AuthState>(
+      listener: (context, state) {
+        if (state is AuthFailure) {
+          AppToast.toast(state.errMessage);
+        }
+        if (state is AuthSuccess) {
+          AppRoute.goReplace(context, HomeView.nameRoute);
+        }
+      },
+      builder: (context, state) {
+        return CustomBtnAuth(
+            title: title,
+            onPressed: onPressed,
+            isLoading: state is AuthLoading ? true : false,
+            backgroundColor: backgroundColor,
+            style: style);
+      },
     );
   }
 }

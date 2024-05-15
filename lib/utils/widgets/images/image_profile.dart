93a9c242956237f497a4a88067e5abea0e5eabd6
khaled-dreat <dreatkhaled@gmail.com>
2024-05-15 17:15:27 +0300

    Add ImageProfile widget for displaying user profile image
    Create a new ImageProfile widget for displaying user profile images with
    loading and error handling capabilities. This widget uses a ClipOval and
    NetworkImage to render the image.

---
+++ lib/utils/widgets/images/image_profile.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import '../../constant/app_image.dart';
+import '../loading/app_loading.dart';
+
+class ImageProfile extends StatelessWidget {
+  const ImageProfile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipOval(
+      child: Image(
+        height: 45,
+        image: const NetworkImage(
+            "https://www.upwork.com/profile-portraits/c1omXvA02zZEGdCF4IZ5XofBUJ1xqhzdhCztiABT7h5Sggh_2vOyt46gOs6UfftAQ9"),
+        errorBuilder: (context, error, stackTrace) => const Image(
+          image: AssetImage(Assets.svgAccount),
+        ),
+        loadingBuilder: (context, child, loadingProgress) {
+          return loadingProgress == null
+              ? child
+              : const Center(
+                  child: AppLoading(loading: TypeLoading.profile),
+                );
+        },
+      ),
+    );
+  }
+}

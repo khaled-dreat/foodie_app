175365192b32d8cf0adfd27970974bd513d03d4c
khaled-dreat <dreatkhaled@gmail.com>
2024-05-15 09:22:10 +0300

    Refactor auth state classes
    Simplify and organize auth state classes in auth_state.dart for better readability.

--- lib/features/auth/presentation/maneg/auth/auth_state.dart
+++ lib/features/auth/presentation/maneg/auth/auth_state.dart
@@ -1,10 +1,26 @@
 part of 'auth_cubit.dart';
 
-abstract class AuthState extends Equatable {
-  const AuthState();
-
-  @override
-  List<Object> get props => [];
-}
+@immutable
+class AuthState {}
 
 class AuthInitial extends AuthState {}
+
+class AuthLoading extends AuthState {}
+
+class AuthFailure extends AuthState {
+  final String errMessage;
+
+  AuthFailure({required this.errMessage});
+}
+
+class AuthGoogleLoading extends AuthState {}
+
+class AuthGoogleSuccess extends AuthState {}
+
+class AuthGoogleFailure extends AuthState {
+  final String errMessage;
+
+  AuthGoogleFailure({required this.errMessage});
+}
+
+class AuthSuccess extends AuthState {}

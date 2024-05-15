319f897302c677977067e5956bb7bce7d75caa15
khaled-dreat <dreatkhaled@gmail.com>
2024-05-15 10:15:08 +0300

    Update the registration view body layout to use separate widgets for form fields and buttons. Implement form validation and submission logic. Add navigation functionality for existing members.

--- lib/features/auth/presentation/pages/registration/widgets/registration_view_body.dart
+++ lib/features/auth/presentation/pages/registration/widgets/registration_view_body.dart
@@ -1,71 +1,90 @@
+import 'dart:developer';
+import "package:flutter_bloc/flutter_bloc.dart";
 import 'package:flutter/material.dart';
-import 'package:foodie_app/features/auth/presentation/pages/widgets/custom_btn_auth.dart';
+import 'package:foodie_app/features/auth/presentation/pages/widgets/auth_field_email.dart';
+import 'package:foodie_app/features/auth/presentation/pages/login/widgets/login_view_body.dart';
+import 'package:foodie_app/features/home/presentation/pages/home/home_view.dart';
 import 'package:foodie_app/utils/constant/app_image.dart';
 import 'package:foodie_app/utils/theme/app_colors.dart';
 import 'package:foodie_app/utils/theme/app_text_style.dart';
 import 'package:svg_flutter/svg_flutter.dart';
-import '../../widgets/custom_auth_text_form_field.dart';
+import '../../../../../../utils/route/app_route.dart';
+import '../../../maneg/auth/auth_cubit.dart';
+import '../../widgets/auth_field_full_name.dart';
+import '../../widgets/auth_field_pass.dart';
+import '../../widgets/auth_footer.dart';
 
 class RegistrationViewBody extends StatelessWidget {
   const RegistrationViewBody({super.key});
+  static final GlobalKey<FormState> keyForm = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 15),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          SvgPicture.asset(AppImages.svgAuthWelcome),
-          const SizedBox(
-            height: 20,
-          ),
-          const CustomAuthTextFormField(
-            isPass: false,
-            hintText: "Enter your full name",
-            label: "Full Name",
-          ),
-          const SizedBox(
-            height: 25,
-          ),
-          const CustomAuthTextFormField(
-            isPass: false,
-            hintText: "Eg namaemail@emailkamu.com",
-            label: "Email address",
-          ),
-          const SizedBox(
-            height: 25,
-          ),
-          const CustomAuthTextFormField(
-            isPass: true,
-            hintText: "**** **** ****",
-            label: "Password",
-          ),
-          const SizedBox(
-            height: 50,
-          ),
-          CustomBtnAuth(
-              title: "Registration",
-              onPressed: () {},
-              backgroundColor: AppColors.green,
-              style: AppTextStyle.styleBold14(context)
-                  .copyWith(color: AppColors.white)),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 100),
-            child: Divider(),
-          ),
-          CustomBtnAuthIcon(
-              title: "Sign up with Google",
-              onPressed: () {},
-              icon: AppImages.svgGoogle,
-              backgroundColor: AppColors.lightGray2,
-              style: AppTextStyle.styleBold14(context)
-                  .copyWith(color: AppColors.black)),
-          const SizedBox(
-            height: 80,
-          ),
-        ],
+    return Form(
+      key: keyForm,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            SvgPicture.asset(AppImages.svgAuthWelcome),
+            const SizedBox(
+              height: 20,
+            ),
+            // * Text Form Filed Full Name
+            const AuthFieldFullName(),
+            const SizedBox(
+              height: 25,
+            ),
+            // * Text Form Filed Email
+            const AuthFieldEmail(),
+            const SizedBox(
+              height: 25,
+            ),
+            // * Text Form Filed Password
+            const AuthFieldPass(),
+            const SizedBox(
+              height: 50,
+            ),
+            // * Registration Btn
+            CustomBtnAuthBlocConsumer(
+                keyForm: keyForm,
+                title: "Registration",
+                onPressed: () async {
+                  AuthCubit? authCubit = context.read<AuthCubit>();
+
+                  if (keyForm.currentState?.validate() ?? false) {
+                    // ✅
+                    log("✅");
+                    keyForm.currentState?.save();
+                    if (await authCubit.authMethod(isSignIn: false) != null) {
+                      AppRoute.goReplace(context, HomeView.nameRoute);
+                    }
+                  }
+                },
+                backgroundColor: AppColors.green,
+                style: AppTextStyle.styleBold14(context)
+                    .copyWith(color: AppColors.white)),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 100),
+              child: Divider(),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            // * Auth Footer (Already a member)
+            AuthFooter(
+                onTap: () {
+                  Navigator.pop(context);
+                },
+                first: "Already a member? ",
+                second: "Login"),
+            const SizedBox(
+              height: 60,
+            ),
+          ],
+        ),
       ),
     );
   }

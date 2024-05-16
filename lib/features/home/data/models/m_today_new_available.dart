40ae2e55542ac618cf4b83ff6c8a646eab64ef5b
khaled-dreat <dreatkhaled@gmail.com>
2024-05-16 22:55:59 +0300

    Add TodayNewAvailableModel class definition
    Added TodayNewAvailableModel class definition with image, title, and description
    fields and constructor.

---
+++ lib/features/home/data/models/m_today_new_available.dart
@@ -0,0 +1,8 @@
+class TodayNewAvailableModel {
+  final String image, title, description;
+  TodayNewAvailableModel({
+    required this.image,
+    required this.title,
+    required this.description,
+  });
+}

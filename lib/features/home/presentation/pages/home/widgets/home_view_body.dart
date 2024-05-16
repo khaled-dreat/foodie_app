58a36ddbb71b4f247fea85bee8579ee3daf01b72
khaled-dreat <dreatkhaled@gmail.com>
2024-05-16 23:07:25 +0300

    Add custom home list header and restaurant image
    Added a custom home list header with title and description for exploring restaurants,
    and included an image of a restaurant within a Card widget.

--- lib/features/home/presentation/pages/home/widgets/home_view_body.dart
+++ lib/features/home/presentation/pages/home/widgets/home_view_body.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:foodie_app/features/home/presentation/pages/home/widgets/custom_home_list_header.dart';
+import 'package:foodie_app/utils/constant/app_image.dart';
 import 'coutom_text_form_srh.dart';
 import 'pest_brand.dart';
 import 'today_new_available_widget/today_new_available_widget.dart';
@@ -9,19 +11,33 @@ class HomeViewBody extends StatelessWidget {
   });
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 20),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
       child: Column(
         children: [
-          CoutomTextFormSrh(),
-          SizedBox(
+          const CoutomTextFormSrh(),
+          const SizedBox(
             height: 30,
           ),
-          PestBrand(),
-          SizedBox(
+          const PestBrand(),
+          const SizedBox(
             height: 30,
           ),
-          TodayNewAvailableWidget()
+          const TodayNewAvailableWidget(),
+          const SizedBox(
+            height: 30,
+          ),
+          const CustomHomeListHeader(
+              title: "Explore Restaurant",
+              description: "Check your city Near by Restaurant"),
+          SizedBox(
+            height: 100,
+            child: Card(
+              child: Row(
+                children: [Image.asset(Assets.svgAmbrosiaHotelRestaurant)],
+              ),
+            ),
+          )
         ],
       ),
     );

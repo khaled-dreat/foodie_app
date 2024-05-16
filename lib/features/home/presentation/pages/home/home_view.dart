ff9c1e1e89332630307de8425b7061b8c52a40ea
khaled-dreat <dreatkhaled@gmail.com>
2024-05-16 22:56:06 +0300

    Refactor home_view.dart file
    Remove unused imports and widgets, refactor AppBar and body structure in HomeView.

--- lib/features/home/presentation/pages/home/home_view.dart
+++ lib/features/home/presentation/pages/home/home_view.dart
@@ -1,13 +1,7 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:foodie_app/features/home/presentation/pages/home/widgets/swipeable.dart';
-import 'package:foodie_app/utils/constant/app_image.dart';
 import 'package:foodie_app/utils/theme/app_colors.dart';
-import 'package:foodie_app/utils/theme/app_text_style.dart';
-import 'package:foodie_app/utils/widgets/images/image_profile.dart';
-import 'package:svg_flutter/svg_flutter.dart';
-import 'package:carousel_slider/carousel_slider.dart';
-import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'widgets/custom_home_appbar.dart';
+import 'widgets/home_view_body.dart';
 
 class HomeView extends StatelessWidget {
   static const String nameRoute = "HomeView";
@@ -15,129 +9,10 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: CustomHomeAppBar(),
-      body: Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 40),
-            child: CoutomTextFormSrh(),
-          ),
-          Swipeable()
-        ],
-      ),
+    return Scaffold(
+      backgroundColor: AppColors.lightGray2,
+      appBar: const CustomHomeAppBar(),
+      body: const HomeViewBody(),
     );
   }
-}
-
-class SSS extends StatefulWidget {
-  const SSS({super.key});
-
-  @override
-  State<SSS> createState() => _SSSState();
-}
-
-class _SSSState extends State<SSS> {
-  final myitems = [
-    Image.asset('images/image01.png'),
-    Image.asset('images/image02.png'),
-    Image.asset('images/image03.png'),
-    Image.asset('images/image04.png'),
-    Image.asset('images/image05.png'),
-    Image.asset('images/image06.png'),
-  ];
-
-  int myCurrentIndex = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        CarouselSlider(
-          options: CarouselOptions(
-            autoPlay: true,
-            height: 200,
-            autoPlayCurve: Curves.fastOutSlowIn,
-            autoPlayAnimationDuration: const Duration(milliseconds: 800),
-            autoPlayInterval: const Duration(seconds: 2),
-            enlargeCenterPage: true,
-            aspectRatio: 2.0,
-            onPageChanged: (index, reason) {
-              setState(() {
-                myCurrentIndex = index;
-              });
-            },
-          ),
-          items: myitems,
-        ),
-        AnimatedSmoothIndicator(
-          activeIndex: myCurrentIndex,
-          count: myitems.length,
-          effect: WormEffect(
-            dotHeight: 8,
-            dotWidth: 8,
-            spacing: 10,
-            dotColor: Colors.grey.shade200,
-            activeDotColor: Colors.grey.shade900,
-            paintStyle: PaintingStyle.fill,
-          ),
-        )
-      ],
-    );
-  }
-}
-
-class CoutomTextFormSrh extends StatelessWidget {
-  const CoutomTextFormSrh({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return TextFormField(
-      decoration: InputDecoration(
-          filled: true,
-          prefixIcon: Icon(
-            Icons.search,
-            color: AppColors.silver,
-            size: 25,
-          ),
-          hintText: "Search",
-          hintStyle: AppTextStyle.styleRegular13(context)
-              .copyWith(color: AppColors.silver, fontSize: 14),
-          fillColor: AppColors.silverGray,
-          enabledBorder: outlineInputBorder(),
-          focusedBorder: outlineInputBorder(),
-          border: outlineInputBorder()),
-    );
-  }
-
-  OutlineInputBorder outlineInputBorder() {
-    return OutlineInputBorder(
-        borderRadius: BorderRadius.circular(15),
-        borderSide: BorderSide(color: Colors.transparent));
-  }
-}
-
-class CustomHomeAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const CustomHomeAppBar({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return AppBar(
-      leading: Padding(
-        padding: const EdgeInsets.all(15),
-        child: SvgPicture.asset(
-          Assets.svgMune,
-        ),
-      ),
-      actions: [
-        Padding(
-          padding: const EdgeInsets.only(right: 15),
-          child: ImageProfile(),
-        )
-      ],
-    );
-  }
-
-  @override
-  Size get preferredSize => Size.fromHeight(AppBar().preferredSize.height);
 }

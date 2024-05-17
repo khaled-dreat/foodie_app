66a184aa06f300829c6f9eb2a6d7822797c517c3
khaled-dreat <dreatkhaled@gmail.com>
2024-05-17 10:01:43 +0300

    Add custom bottom navigation bar widget
    Import and use the custom bottom navigation bar widget in the home view for better
    code organization and readability.

--- lib/features/home/presentation/pages/home/home_view.dart
+++ lib/features/home/presentation/pages/home/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:foodie_app/utils/theme/app_colors.dart';
+import 'widgets/custom_bottom_navigation_bar.dart';
 import 'widgets/custom_home_appbar.dart';
 import 'widgets/home_view_body.dart';
 
@@ -13,38 +14,7 @@ class HomeView extends StatelessWidget {
       backgroundColor: AppColors.lightGray2,
       appBar: const CustomHomeAppBar(),
       body: const HomeViewBody(),
-      bottomNavigationBar: ClipRRect(
-        borderRadius: BorderRadius.only(
-            topRight: Radius.circular(25), topLeft: Radius.circular(25)),
-        child: BottomNavigationBar(
-            selectedItemColor: AppColors.green,
-            elevation: 15,
-            backgroundColor: AppColors.white,
-            currentIndex: 1,
-            items: [
-              BottomNavigationBarItem(
-                label: "",
-                icon: Icon(
-                  Icons.home_outlined,
-                  size: 30,
-                ),
-              ),
-              BottomNavigationBarItem(
-                label: "",
-                icon: Icon(
-                  Icons.sticky_note_2_outlined,
-                  size: 25,
-                ),
-              ),
-              BottomNavigationBarItem(
-                label: "",
-                icon: Icon(
-                  Icons.person_outline_outlined,
-                  size: 25,
-                ),
-              )
-            ]),
-      ),
+      bottomNavigationBar: const CustomBottomNavigationBar(),
     );
   }
 }

9eabc7d90bb185a5facc4f0c36574f85775d4da5
khaled-dreat <dreatkhaled@gmail.com>
2024-05-17 10:02:12 +0300

    Add ExploreRestaurantWidget with restaurant data and cards
    Add a new ExploreRestaurantWidget that displays a list of restaurant cards with
    data such as image, title, and place. This widget is used to explore nearby
    restaurants in the app.

---
+++ lib/features/home/presentation/pages/home/widgets/explore_restaurant_widget/explore_restaurant_widget.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../../../utils/constant/app_image.dart';
+import '../../../../../data/models/m_restaurant.dart';
+import '../custom_home_list_header.dart';
+import '../home_view_body.dart';
+import 'explore_restaurant_card.dart';
+
+class ExploreRestaurantWidget extends StatelessWidget {
+  const ExploreRestaurantWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    List<RestaurantModel> restaurantModel = [
+      RestaurantModel(
+          image: Assets.svgAmbrosiaHotelRestaurant,
+          title: "Ambrosia Hotel & Restaurant",
+          place: "Zakir Hossain Rd, Chittagong"),
+      RestaurantModel(
+          image: Assets.svgTavaRestaurant,
+          title: "Tava Restaurant",
+          place: "kazi Deiry, Taiger Pass Chittagong"),
+      RestaurantModel(
+          image: Assets.svgHaatkhola,
+          title: "Haatkhola",
+          place: "6 Surson Road, Chittagong")
+    ];
+    return Column(
+      children: [
+        const CustomHomeListHeader(
+            title: "Explore Restaurant",
+            description: "Check your city Near by Restaurant"),
+        const SizedBox(
+          height: 20,
+        ),
+        Column(
+          children: restaurantModel.map(
+            (e) {
+              return ExploreRestaurantCard(
+                image: e.image,
+                place: e.place,
+                title: e.title,
+              );
+            },
+          ).toList(),
+        )
+      ],
+    );
+  }
+}

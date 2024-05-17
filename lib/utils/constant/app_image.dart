1d397a2ee4a6ac02497bce80182b6fe963046a15
khaled-dreat <dreatkhaled@gmail.com>
2024-05-17 10:01:37 +0300

    Add new SVG icons for Open House and Visit Restaurant
    Added new SVG icons for Open House and Visit Restaurant in the assets folder.

--- lib/utils/constant/app_image.dart
+++ lib/utils/constant/app_image.dart
@@ -61,6 +61,10 @@ class Assets {
   /// assets/svg/on_bording_3.svg
   static const String svgOnBording3 = "assets/svg/on_bording_3.svg";
 
+  /// Assets for svgOpenHouseIcon
+  /// assets/svg/OpenHouseIcon.svg
+  static const String svgOpenHouseIcon = "assets/svg/OpenHouseIcon.svg";
+
   /// Assets for svgOrderCard
   /// assets/svg/order_card.svg
   static const String svgOrderCard = "assets/svg/order_card.svg";
@@ -85,8 +89,16 @@ class Assets {
   /// assets/svg/TavaRestaurant.jpg
   static const String svgTavaRestaurant = "assets/svg/TavaRestaurant.jpg";
 
+  /// Assets for svgTavaRestaurant2
+  /// assets/svg/TavaRestaurant2.jpg
+  static const String svgTavaRestaurant2 = "assets/svg/TavaRestaurant2.jpg";
+
   /// Assets for svgUnitedStates
   /// assets/svg/united_states.png
   static const String svgUnitedStates = "assets/svg/united_states.png";
+
+  /// Assets for svgVisitRestaurantIcon
+  /// assets/svg/VisitRestaurantIcon.svg
+  static const String svgVisitRestaurantIcon = "assets/svg/VisitRestaurantIcon.svg";
 }
 

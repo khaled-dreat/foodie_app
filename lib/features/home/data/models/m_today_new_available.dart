47053364e65a5575b1bc37cef4b3fb2a487c78fb
khaled-dreat <dreatkhaled@gmail.com>
2024-05-17 09:02:41 +0300

    Update TodayNewAvailableModel properties
    Change the 'description' property to 'place' in the TodayNewAvailableModel
    class for consistency and clarity.

--- lib/features/home/data/models/m_today_new_available.dart
+++ lib/features/home/data/models/m_today_new_available.dart
@@ -1,8 +1,8 @@
 class TodayNewAvailableModel {
-  final String image, title, description;
+  final String image, title, place;
   TodayNewAvailableModel({
     required this.image,
     required this.title,
-    required this.description,
+    required this.place,
   });
 }

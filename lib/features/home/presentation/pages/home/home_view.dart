ac48af7c8308c44507342caf9ef69d2a2df3b2b8
khaled-dreat <dreatkhaled@gmail.com>
2024-05-17 09:02:47 +0300

    Add bottom navigation bar to HomeView
    Add a bottom navigation bar with icons for different sections in the HomeView
    page. This navigation bar provides easy access to key features of the app.

--- lib/features/home/presentation/pages/home/home_view.dart
+++ lib/features/home/presentation/pages/home/home_view.dart
@@ -13,6 +13,38 @@ class HomeView extends StatelessWidget {
       backgroundColor: AppColors.lightGray2,
       appBar: const CustomHomeAppBar(),
       body: const HomeViewBody(),
+      bottomNavigationBar: ClipRRect(
+        borderRadius: BorderRadius.only(
+            topRight: Radius.circular(25), topLeft: Radius.circular(25)),
+        child: BottomNavigationBar(
+            selectedItemColor: AppColors.green,
+            elevation: 15,
+            backgroundColor: AppColors.white,
+            currentIndex: 1,
+            items: [
+              BottomNavigationBarItem(
+                label: "",
+                icon: Icon(
+                  Icons.home_outlined,
+                  size: 30,
+                ),
+              ),
+              BottomNavigationBarItem(
+                label: "",
+                icon: Icon(
+                  Icons.sticky_note_2_outlined,
+                  size: 25,
+                ),
+              ),
+              BottomNavigationBarItem(
+                label: "",
+                icon: Icon(
+                  Icons.person_outline_outlined,
+                  size: 25,
+                ),
+              )
+            ]),
+      ),
     );
   }
 }

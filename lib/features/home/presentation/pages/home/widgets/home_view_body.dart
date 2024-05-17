f056d5058a9ff4e3192c2b24f439eb14e24856db
khaled-dreat <dreatkhaled@gmail.com>
2024-05-17 10:01:55 +0300

    Add ExploreRestaurantWidget to HomeViewBody
    Add ExploreRestaurantWidget to the list of widgets displayed in HomeViewBody. This widget will enhance the user experience by providing access to explore nearby restaurants.

--- lib/features/home/presentation/pages/home/widgets/home_view_body.dart
+++ lib/features/home/presentation/pages/home/widgets/home_view_body.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:foodie_app/features/home/presentation/pages/home/widgets/custom_home_list_header.dart';
-import 'package:foodie_app/utils/constant/app_image.dart';
 import 'coutom_text_form_srh.dart';
+import 'explore_restaurant_widget/explore_restaurant_widget.dart';
 import 'pest_brand.dart';
 import 'today_new_available_widget/today_new_available_widget.dart';
 
@@ -11,34 +10,26 @@ class HomeViewBody extends StatelessWidget {
   });
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20),
-      child: Column(
-        children: [
-          const CoutomTextFormSrh(),
-          const SizedBox(
-            height: 30,
-          ),
-          const PestBrand(),
-          const SizedBox(
-            height: 30,
-          ),
-          const TodayNewAvailableWidget(),
-          const SizedBox(
-            height: 30,
-          ),
-          const CustomHomeListHeader(
-              title: "Explore Restaurant",
-              description: "Check your city Near by Restaurant"),
-          SizedBox(
-            height: 100,
-            child: Card(
-              child: Row(
-                children: [Image.asset(Assets.svgAmbrosiaHotelRestaurant)],
-              ),
+    return const SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            CoutomTextFormSrh(),
+            SizedBox(
+              height: 30,
             ),
-          )
-        ],
+            PestBrand(),
+            SizedBox(
+              height: 30,
+            ),
+            TodayNewAvailableWidget(),
+            SizedBox(
+              height: 30,
+            ),
+            ExploreRestaurantWidget()
+          ],
+        ),
       ),
     );
   }

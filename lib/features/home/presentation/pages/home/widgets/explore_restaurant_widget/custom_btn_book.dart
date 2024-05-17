c6b31db70e8f13fa307fe44fb2303350c8c14dd1
khaled-dreat <dreatkhaled@gmail.com>
2024-05-17 10:02:01 +0300

    Add CustomBtnBook widget for booking restaurants

---
+++ lib/features/home/presentation/pages/home/widgets/explore_restaurant_widget/custom_btn_book.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../../../utils/widgets/loading/app_loading.dart';
+
+class CustomBtnBook extends StatelessWidget {
+  const CustomBtnBook({
+    super.key,
+    required this.title,
+    required this.onPressed,
+    required this.backgroundColor,
+    required this.style,
+    this.isLoading = false,
+  });
+  final Color backgroundColor;
+  final String title;
+  final TextStyle style;
+  final bool isLoading;
+
+  final void Function()? onPressed;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 110,
+      height: 40,
+      child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+              elevation: 0,
+              backgroundColor: backgroundColor,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12))),
+          onPressed: onPressed,
+          child: isLoading
+              ? const AppLoading(
+                  loading: TypeLoading.send,
+                )
+              : Text(
+                  title,
+                  style: style,
+                )),
+    );
+  }
+}

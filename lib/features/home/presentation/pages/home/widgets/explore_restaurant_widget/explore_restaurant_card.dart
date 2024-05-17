e0856b5813b589cb0ed7f6393b080dcc5c0e1c53
khaled-dreat <dreatkhaled@gmail.com>
2024-05-17 10:02:07 +0300

    Add ExploreRestaurantCard widget for home page
    Add ExploreRestaurantCard widget to display restaurant information in the home page.

---
+++ lib/features/home/presentation/pages/home/widgets/explore_restaurant_widget/explore_restaurant_card.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:svg_flutter/svg_flutter.dart';
+
+import '../../../../../../../utils/constant/app_image.dart';
+import '../../../../../../../utils/theme/app_colors.dart';
+import '../../../../../../../utils/theme/app_text_style.dart';
+import '../home_view_body.dart';
+import 'custom_btn_book.dart';
+
+class ExploreRestaurantCard extends StatelessWidget {
+  const ExploreRestaurantCard({
+    super.key,
+    required this.image,
+    required this.title,
+    required this.place,
+  });
+  final String image, title, place;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 100,
+      child: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        child: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset(image),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: AppTextStyle.styleSemiBold16(context),
+                ),
+                SizedBox(
+                  width: 240,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 3),
+                        child: SvgPicture.asset(
+                          Assets.svgMarker,
+                          height: 14,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      Expanded(
+                        child: Text(
+                          place,
+                          style: AppTextStyle.styleRegular13(context)
+                              .copyWith(fontSize: 10),
+                        ),
+                      ),
+                      CustomBtnBook(
+                          title: "Book",
+                          onPressed: () {},
+                          backgroundColor: AppColors.green,
+                          style: AppTextStyle.styleSemiBold12(context)
+                              .copyWith(color: AppColors.white))
+                    ],
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}

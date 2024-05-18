ad1650234c00bbd752e45404eba2fe89e29141f8
khaled-dreat <dreatkhaled@gmail.com>
2024-05-18 22:14:18 +0300

    Remove unused code for search bar and model
    - Removed unused code for search bar and model
    - Deleted TodayNewAvailableModel and CoutomTextFormSrh files
    - Code cleanup and optimization

--- lib/features/home/data/models/m_today_new_available.dart
+++
@@ -1,8 +0,0 @@
-class TodayNewAvailableModel {
-  final String image, title, place;
-  TodayNewAvailableModel({
-    required this.image,
-    required this.title,
-    required this.place,
-  });
-}

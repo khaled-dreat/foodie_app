8a400e11b6bcb5d1102b7e703070596128252be8
khaled-dreat <dreatkhaled@gmail.com>
2024-05-18 18:33:50 +0300

    Add ProfileViewBody widget to display user profile information and settings options.

---
+++ lib/features/profile/presentation/pages/widgets/profile_view_body.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../utils/theme/app_colors.dart';
+import '../../../../../utils/theme/app_text_style.dart';
+import '../../../../home/presentation/pages/home/widgets/explore_restaurant_widget/custom_btn_book.dart';
+import 'account_setting_section.dart';
+import 'app_settings_section.dart';
+import 'notification_section.dart';
+
+class ProfileViewBody extends StatelessWidget {
+  const ProfileViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const SizedBox(
+          height: 100,
+        ),
+        const NotificationSection(),
+        const AccountSettingSection(),
+        const AppSettingsSection(),
+        const SizedBox(
+          height: 80,
+        ),
+        SizedBox(
+          width: 150,
+          child: CustomBtnBook(
+              title: "Logout",
+              onPressed: () {},
+              backgroundColor: AppColors.crimson,
+              style: AppTextStyle.styleSemiBold16(context)
+                  .copyWith(color: AppColors.white)),
+        )
+      ],
+    );
+  }
+}

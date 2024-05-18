19ddb35c926f8d672b98d266c56640c3d34d9766
khaled-dreat <dreatkhaled@gmail.com>
2024-05-18 22:14:32 +0300

    Add rating field to RestaurantModel class

--- lib/features/home/data/models/m_restaurant.dart
+++ lib/features/home/data/models/m_restaurant.dart
@@ -1,6 +1,7 @@
 class RestaurantModel {
-  final String image, title, place;
+  final String image, title, place, rating;
   RestaurantModel({
+    required this.rating,
     required this.image,
     required this.title,
     required this.place,

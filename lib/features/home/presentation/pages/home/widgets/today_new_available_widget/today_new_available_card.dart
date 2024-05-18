dc3a480cb7cb2cd9d4d4974ab68eba6b710cfd69
khaled-dreat <dreatkhaled@gmail.com>
2024-05-18 22:14:52 +0300

    Refactor TodayNewAvailableCard widget to FoodCard widget
    - Renamed TodayNewAvailableCard to FoodCard for better naming convention
    - Increased the height of the card to accommodate additional content
    - Removed unused import and variable
    - Added elevation to the card for better visual hierarchy
    - Added InkWell with icon to the restaurant name for future functionality
    - Updated the restaurant name variable to better reflect its purpose

--- lib/features/home/presentation/pages/home/widgets/today_new_available_widget/today_new_available_card.dart
+++ lib/features/home/presentation/pages/home/widgets/today_new_available_widget/today_new_available_card.dart
@@ -1,24 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:foodie_app/utils/theme/app_colors.dart';
 import 'package:svg_flutter/svg_flutter.dart';
 
 import '../../../../../../../utils/constant/app_image.dart';
 import '../../../../../../../utils/theme/app_text_style.dart';
 
-class TodayNewAvailableCard extends StatelessWidget {
-  const TodayNewAvailableCard({
+class FoodCard extends StatelessWidget {
+  const FoodCard({
     super.key,
     required this.image,
     required this.title,
-    required this.place,
+    required this.restaurant,
   });
-  final String image, title, place;
+  final String image, title, restaurant;
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       width: 180,
-      height: 230,
+      height: 250,
       child: Card(
+        elevation: 3,
+        //  color: AppColors.platinumGray,
         child: Padding(
           padding: const EdgeInsets.all(10),
           child: Column(
@@ -40,22 +44,29 @@ class TodayNewAvailableCard extends StatelessWidget {
               Row(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.only(top: 3),
-                    child: SvgPicture.asset(
-                      Assets.svgMarker,
-                      height: 14,
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 5,
-                  ),
                   Expanded(
                     child: Text(
-                      place,
+                      restaurant,
                       style: AppTextStyle.styleMedium12(context),
                     ),
-                  )
+                  ),
+                  InkWell(
+                    onTap: () {},
+                    child: Container(
+                      height: 30,
+                      width: 30,
+                      alignment: AlignmentDirectional.center,
+                      decoration: ShapeDecoration(
+                        color: AppColors.green,
+                        shape: const OvalBorder(),
+                      ),
+                      child: Icon(
+                        Icons.add,
+                        size: 20,
+                        color: AppColors.white,
+                      ),
+                    ),
+                  ),
                 ],
               ),
               const SizedBox(

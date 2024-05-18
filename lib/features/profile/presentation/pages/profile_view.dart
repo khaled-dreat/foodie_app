74887ecf5b22ee4dc4226f8ce2a88042eabf95cf
khaled-dreat <dreatkhaled@gmail.com>
2024-05-18 09:01:19 +0300

    Add profile feature files
    Added necessary files for the profile feature including data sources, models, repositories, entities, use cases, presentation cubit, presentation pages, and presentation widgets.

---
+++ lib/features/profile/presentation/pages/profile_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ProfileView extends StatelessWidget {
+  static const String nameRoute = "ProfileView";
+  const ProfileView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

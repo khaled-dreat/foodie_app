c0d972432bb97244515ad804f13246eb2588ec10
khaled-dreat <dreatkhaled@gmail.com>
2024-05-18 18:33:22 +0300

    Add background color and profile view body widget to ProfileView page

--- lib/features/profile/presentation/pages/profile_view.dart
+++ lib/features/profile/presentation/pages/profile_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:foodie_app/utils/theme/app_colors.dart';
+import 'widgets/profile_view_body.dart';
 
 class ProfileView extends StatelessWidget {
   static const String nameRoute = "ProfileView";
@@ -6,6 +8,9 @@ class ProfileView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: AppColors.lightGray2,
+      body: const ProfileViewBody(),
+    );
   }
 }

6598b23be451872ada7b99b10979320860c2d541
khaled-dreat <dreatkhaled@gmail.com>
2024-05-19 18:56:55 +0300

    Refactor FoodModel class
    - Add new properties to the FoodModel class
    - Implement fromJson and toJson methods for serialization
    - Implement copyWith method for immutability and ease of updating properties

--- lib/features/home/data/models/m_food.dart
+++ lib/features/home/data/models/m_food.dart
@@ -1,8 +1,87 @@
 class FoodModel {
-  final String image, title, restaurant;
+  final int id;
+  final String name;
+  final String img;
+  final bool active;
+  final double price;
+  final bool isFreeDelivery;
+  final String details;
+  final double rating;
+  final String size;
+  final int restaurantID;
+  final List<String> offers;
+
   FoodModel({
-    required this.image,
-    required this.title,
-    required this.restaurant,
+    required this.id,
+    required this.name,
+    required this.img,
+    required this.active,
+    required this.price,
+    required this.isFreeDelivery,
+    required this.details,
+    required this.rating,
+    required this.size,
+    required this.restaurantID,
+    required this.offers,
   });
+
+  factory FoodModel.fromJson(Map<String, dynamic> json) {
+    return FoodModel(
+      id: json['id'],
+      name: json['name'],
+      img: json['img'],
+      active: json['active'],
+      price: json['price'].toDouble(),
+      isFreeDelivery: json['isFreeDelivery'],
+      details: json['details'],
+      rating: json['rating'].toDouble(),
+      size: json['size'],
+      restaurantID: json['restaurantID'],
+      offers: List<String>.from(json['offers']),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'img': img,
+      'active': active,
+      'price': price,
+      'isFreeDelivery': isFreeDelivery,
+      'details': details,
+      'rating': rating,
+      'size': size,
+      'restaurantID': restaurantID,
+      'offers': offers,
+    };
+  }
+
+  FoodModel copyWith({
+    int? id,
+    String? name,
+    String? img,
+    bool? active,
+    double? price,
+    bool? isFreeDelivery,
+    String? details,
+    double? rating,
+    String? size,
+    int? restaurantID,
+    List<String>? offers,
+  }) {
+    return FoodModel(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      img: img ?? this.img,
+      active: active ?? this.active,
+      price: price ?? this.price,
+      isFreeDelivery: isFreeDelivery ?? this.isFreeDelivery,
+      details: details ?? this.details,
+      rating: rating ?? this.rating,
+      size: size ?? this.size,
+      restaurantID: restaurantID ?? this.restaurantID,
+      offers: offers ?? this.offers,
+    );
+  }
 }

f361ff9cae23b4e123cc0f6324fb4537ecebf28e
khaled-dreat <dreatkhaled@gmail.com>
2024-05-20 09:06:50 +0300

    Refactor search cubit to use search use case
    This commit refactors the search cubit to use the search use case for searching food items. The search use case returns a result that is either a list of food entities or a failure. The cubit emits the appropriate state based on the result of the use case.

--- lib/features/search/presentation/cubit/search_cubit.dart
+++ lib/features/search/presentation/cubit/search_cubit.dart
@@ -1,8 +1,23 @@
 import 'package:bloc/bloc.dart';
-import 'package:equatable/equatable.dart';
-
+import 'package:foodie_app/features/search/domain/entities/food_entite.dart';
+import 'package:foodie_app/features/search/domain/usecases/srh_food_use_case.dart';
 part 'search_state.dart';
 
 class SearchCubit extends Cubit<SearchState> {
-  SearchCubit() : super(SearchInitial());
+  SearchCubit({required this.srhFoodUseCase}) : super(SearchInitial());
+
+  final SrhFoodUseCase srhFoodUseCase;
+
+  Future<void> srhFood({required String srhKey}) async {
+    emit(SearchLoading());
+    var result = await srhFoodUseCase.call(srhKey);
+    result.fold(
+      (failure) {
+        emit(SearchFailure(errMessage: failure.message));
+      },
+      (foodList) {
+        emit(SearchSuccess(foodList: foodList));
+      },
+    );
+  }
 }

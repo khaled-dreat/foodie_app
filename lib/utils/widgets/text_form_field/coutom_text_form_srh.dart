df1ca962bee959ae631176117070d777416df5d8
khaled-dreat <dreatkhaled@gmail.com>
2024-05-20 09:07:05 +0300

    Add onTap and readOnly properties to CoutomTextFormSrh
    This commit adds the onTap and readOnly properties to the CoutomTextFormSrh widget. The onTap property allows for a function to be executed when the widget is tapped, while the readOnly property allows for the widget to be set as read-only. These properties provide more flexibility and customization options for the widget. Additionally, imports for the srh_view.dart and app_route.dart files were added.

--- lib/utils/widgets/text_form_field/coutom_text_form_srh.dart
+++ lib/utils/widgets/text_form_field/coutom_text_form_srh.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:foodie_app/features/search/presentation/pages/srh_view.dart';
+import 'package:foodie_app/utils/route/app_route.dart';
 
 import '../../../../../../utils/theme/app_colors.dart';
 import '../../../../../../utils/theme/app_text_style.dart';
 
 class CoutomTextFormSrh extends StatelessWidget {
-  const CoutomTextFormSrh({super.key});
-
+  const CoutomTextFormSrh({super.key, this.onTap, this.readOnly = false});
+  final Function()? onTap;
+  final bool readOnly;
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      onTap: onTap,
+      readOnly: readOnly,
       decoration: InputDecoration(
           filled: true,
           prefixIcon: Icon(

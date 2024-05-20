028656315ca183c838a0e4448997138d8802deb2
khaled-dreat <dreatkhaled@gmail.com>
2024-05-20 09:06:30 +0300

    Add dynamic data to RecentKeywordsWidget using Firestore stream
    Added import statements for cloud_firestore and firebase_core packages. Also imported AppFirebaseKey for accessing the collection and document fields. Replaced the SingleChildScrollView with a StreamBuilder that listens to the Firestore collection and displays the data in a ListView. This makes the RecentKeywordsWidget dynamic and updates in real-time.

--- lib/utils/widgets/recent_keywords_widget/recent_keywords_widget.dart
+++ lib/utils/widgets/recent_keywords_widget/recent_keywords_widget.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:foodie_app/utils/constant/app_firebase_key.dart';
 
 import '../../theme/app_colors.dart';
 import '../../theme/app_text_style.dart';
@@ -24,11 +27,22 @@ class RecentKeywordsWidget extends StatelessWidget {
         SizedBox(
           height: 10,
         ),
-        SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Row(
-            children: recentKeywords.map(
-              (e) {
+        StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance
+              .collection(AppFirebaseKey.foodModels)
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return Text('Something went wrong');
+            }
+
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Text("Loading");
+            }
+            return ListView.builder(
+              itemCount: snapshot.data?.docs.length,
+              itemBuilder: (context, index) {
+                var data = snapshot.data?.docs[index];
                 return Container(
                   margin: EdgeInsets.only(right: 10),
                   padding: EdgeInsets.symmetric(horizontal: 18, vertical: 10),
@@ -38,12 +52,12 @@ class RecentKeywordsWidget extends StatelessWidget {
                         width: 2.0,
                       ),
                       borderRadius: BorderRadius.circular(25)),
-                  child: Text(e),
+                  child: Text(data![AppFirebaseKey.name]),
                 );
               },
-            ).toList(),
-          ),
-        ),
+            );
+          },
+        )
       ],
     );
   }

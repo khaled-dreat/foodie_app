9a8d6c5203333964f0375cd2a26258d800309c08
khaled-dreat <dreatkhaled@gmail.com>
2024-05-20 09:06:57 +0300

    Refactor search state in search cubit
    - Remove unused Equatable mixin from SearchState abstract class
    - Add SearchLoading and SearchFailure states to handle loading and error scenarios respectively
    - Add SearchSuccess state to handle successful search results with a list of FoodEntity objects
    - Update SearchState subclasses to include necessary parameters

--- lib/features/search/presentation/cubit/search_state.dart
+++ lib/features/search/presentation/cubit/search_state.dart
@@ -1,10 +1,19 @@
 part of 'search_cubit.dart';
 
-abstract class SearchState extends Equatable {
-  const SearchState();
-
-  @override
-  List<Object> get props => [];
-}
+abstract class SearchState {}
 
 class SearchInitial extends SearchState {}
+
+class SearchLoading extends SearchState {}
+
+class SearchFailure extends SearchState {
+  final String errMessage;
+
+  SearchFailure({required this.errMessage});
+}
+
+class SearchSuccess extends SearchState {
+  final List<FoodEntity> foodList;
+
+  SearchSuccess({required this.foodList});
+}

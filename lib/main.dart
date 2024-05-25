4d8c809f8916cfdb110c2a5ce92d518c401de44a
khaled-dreat <dreatkhaled@gmail.com>
2024-05-25 15:33:58 +0300

    Add setup service locator to main.dart
    This commit adds the setup service locator to main.dart to initialize the service locator for dependency injection. It also includes a comment indicating the initialization of Firebase.

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,17 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'firebase_options.dart';
 import 'foodie_app/app_start.dart';
+import 'utils/setup_service_locator/setup_service_locator.dart';
 import 'utils/widgets/simple_bloc_observer/simple_bloc_observer.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  // * FireBase
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  setupServiceLocatorSrh();
+
   Bloc.observer = SimpleBlocObserver();
 
   runApp(const AppStart());

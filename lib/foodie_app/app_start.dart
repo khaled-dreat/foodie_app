394af5cc4cc70359bb2b950f49d361ecb8018388
khaled-dreat <dreatkhaled@gmail.com>
2024-05-25 15:35:49 +0300

    Add search feature to AppStart
    This commit adds the search feature to the AppStart screen by importing the necessary files and creating a SearchCubit provider. The SrhFoodUseCase is also initialized with the SrhRepo dependency obtained from the service locator.

--- lib/foodie_app/app_start.dart
+++ lib/foodie_app/app_start.dart
@@ -3,7 +3,12 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:foodie_app/features/auth/presentation/maneg/auth/auth_cubit.dart';
 import 'package:foodie_app/features/auth/presentation/maneg/wrapper/wrapper_cubit.dart';
 import 'package:foodie_app/features/auth/presentation/pages/wrapper/widgets/wrapper_stream_builder.dart';
+import 'package:foodie_app/features/search/data/repositories/srh_reop_impl.dart';
+import 'package:foodie_app/features/search/domain/usecases/srh_food_use_case.dart';
+import 'package:foodie_app/features/search/presentation/cubit/search_cubit.dart';
+import 'package:foodie_app/utils/setup_service_locator/setup_service_locator.dart';
 import 'package:nested/nested.dart';
+import '../features/search/domain/repositories/srh_reop.dart';
 import '../utils/route/app_route.dart';
 import '../utils/theme/app_theme_choose.dart';
 
@@ -31,6 +36,13 @@ class AppStart extends StatelessWidget {
 
 List<SingleChildWidget> get providers {
   return [
+    BlocProvider(
+      create: (context) {
+        return SearchCubit(
+          srhFoodUseCase: SrhFoodUseCase(srhRepo: getIt.get<SrhRepo>()),
+        );
+      },
+    ),
     BlocProvider(
       create: (context) {
         return WrapperCubit();

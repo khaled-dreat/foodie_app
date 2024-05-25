50867855deacce3e5372d9dfb5abb2e1b2e51f36
khaled-dreat <dreatkhaled@gmail.com>
2024-05-25 15:35:08 +0300

    Refactor SrhFoodUseCase to return a stream of QuerySnapshot instead of Future<Either>

--- lib/features/search/domain/usecases/srh_food_use_case.dart
+++ lib/features/search/domain/usecases/srh_food_use_case.dart
@@ -1,16 +1,12 @@
-import 'package:foodie_app/features/search/domain/entities/food_entite.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:foodie_app/features/search/domain/repositories/srh_reop.dart';
-import 'package:dartz/dartz.dart';
-import '../../../../utils/constant/app_failure.dart';
-import '../../../../utils/usecase/use_case.dart';
 
-class SrhFoodUseCase extends UseCase<List<FoodEntity>, String> {
+class SrhFoodUseCase {
   final SrhRepo srhRepo;
 
   SrhFoodUseCase({required this.srhRepo});
 
-  @override
-  Future<Either<Failure, List<FoodEntity>>> call([String? param]) async {
-    return await srhRepo.srhQuery(srhText: param!);
+  Stream<QuerySnapshot<Map<String, dynamic>>> call([String? param]) {
+    return srhRepo.srhQuery(param!);
   }
 }
